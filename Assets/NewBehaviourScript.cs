8cc2fe0343fdb8a8429c75431e36b9c76407bc6a
Elena Slavina <55747006+Singingsnowbirdie@users.noreply.github.com>
2022-03-29 12:11:27 +0300

    Project loaded

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    bool someBool;
+    int someInt = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(someBool);
+        Debug.Log(someInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
